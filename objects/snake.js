1b06f9035719fee22727bf658b6417f1ba6f64ff
Tim Kreuz <tim.kreuz@gmail.com>
2020-07-21 18:59:58 -0500

    small updates

--- objects/snake.js
+++ objects/snake.js
@@ -9,7 +9,7 @@ const LEFT = "left";
 const RIGHT = "right";
 
 export function getSnake(canvas) {
-    var snake = { type: "complex", speed: .5, dir: DOWN, nextDir: NONE, headSize:0, parts: [] };
+    var snake = { type: "complex", speed: 1, dir: DOWN, nextDir: NONE, headSize:0, parts: [] };
     snake.parts = getSegments(snake.speedx, snake.speedy);
     return snake;
 }
@@ -25,10 +25,13 @@ function getSegments(canvas) {
     return [tail, s2, s3, s4, head];
 }
 
+//The snake only moves down.  No changing directions yet.
+//It does not respond to events. But the events are loaded and ready to use.
 export function updateSnake(game) {
 
     var snake = game.snake;
 
+    //register the next Direction.  We won't actually change directions until the current head gets to 100%
     if (snake.nextDir = NONE) {
         if (game.events.upPressed && (snake.dir == LEFT || snake.dir == RIGHT)) snake.nextDir = UP;
         if (game.events.downPressed && (snake.dir == LEFT || snake.dir == RIGHT)) snake.nextDir = DOWN;
@@ -36,18 +39,27 @@ export function updateSnake(game) {
         if (game.events.rightPressed && (snake.dir == UP || snake.dir == DOWN)) snake.nextDir = RIGHT;
     }
 
+    //the head increases by the speed
     snake.headSize = snake.headSize + snake.speed;
     if (snake.headSize >= SEG_SIZE) snake.headSize = SEG_SIZE;
 
-    snake.parts[0].y = snake.parts[0].y + snake.speed;
-    snake.parts[0].height = SEG_SIZE - snake.headSize;
-    snake.parts[snake.parts.length - 1].height = snake.parts[snake.parts.length - 1].height + snake.speed;
+    //subtract a little from the tail
+    var tail = snake.parts[0];
+    tail.y = snake.parts[0].y + snake.speed;
+    tail.height = SEG_SIZE - snake.headSize;
+    //add a little to the head
+    var head = snake.parts[snake.parts.length - 1];
+    head.height = head.height + snake.speed;
 
+    //if the current head is 100% done, then chop of the tail piece (shift) and add a new head (push)
+    //shift removes the first element of an array
+    //push adds a new element to the end of the array
     if (snake.headSize == SEG_SIZE) {
         snake.parts.shift();
         var oldHead = snake.parts[snake.parts.length - 1];
-        var head = { type: "rectangle", x: oldHead.x, y: oldHead.y + SEG_SIZE, width: SEG_SIZE, height: 0, color: BASE_COLOR };
-        snake.parts.push(head);
+        var newHead = { type: "rectangle", x: oldHead.x, y: oldHead.y + SEG_SIZE, width: SEG_SIZE, height: 0, color: BASE_COLOR };
+        if (newHead.y > game.canvas.height) newHead.y = 0;
+        snake.parts.push(newHead);
         snake.headSize = 0;
     } 
 
