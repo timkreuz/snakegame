e7592b1425a6c6d1c4beffc0252e011312cd76b5
Tim Kreuz <tim.kreuz@gmail.com>
2020-07-28 18:57:55 -0500

    good snake

--- controllers/events.js
+++ controllers/events.js
@@ -3,24 +3,17 @@ export function keyDown(game, e) {
     if (e.keyCode == 37) {
         game.events.leftPressed = true;
     }
+    else if (e.keyCode == 38) {
+        game.events.upPressed = true;
+    }
     else if (e.keyCode == 39) {
         game.events.rightPressed = true;
     }
-    else if (e.keyCode == 32) {
-        game.events.spacePressed = true;
+    else if (e.keyCode == 40) {
+        game.events.downPressed = true;
     }
 }
 
 export function keyUp(game, e) {
     
-    if (e.keyCode == 37) {
-        game.events.leftPressed = false;
-    }
-    else if (e.keyCode == 39) {
-        game.events.rightPressed = false;
-    }
-    if (e.keyCode == 32) {
-        game.events.spacePressed = false;
-    }
-
 }
--- objects/snake.js
+++ objects/snake.js
@@ -9,19 +9,19 @@ const LEFT = "left";
 const RIGHT = "right";
 
 export function getSnake(canvas) {
-    var snake = { type: "complex", speed: 2, dir: DOWN, nextDir: NONE, headSize:0, parts: [] };
+    var snake = { type: "complex", speed: 2.0, nextDir: NONE, nextDir2: NONE, headSize: 0, parts: [] };
     snake.parts = getSegments(snake.speedx, snake.speedy);
     return snake;
 }
 
 function getSegments(canvas) {
-    var xStart = 140;
+    var xStart = 160;
     var yStart = 0;
-    var tail = { type: "rectangle", x: xStart, y: yStart + SEG_SIZE * 4, width: SEG_SIZE, height: SEG_SIZE, color: BASE_COLOR };
-    var s2 = { type: "rectangle", x: xStart, y: yStart + SEG_SIZE * 5, width: SEG_SIZE, height: SEG_SIZE, color: BASE_COLOR };
-    var s3 = { type: "rectangle", x: xStart, y: yStart + SEG_SIZE * 6, width: SEG_SIZE, height: SEG_SIZE, color: BASE_COLOR };
-    var s4 = { type: "rectangle", x: xStart, y: yStart + SEG_SIZE * 7, width: SEG_SIZE, height: SEG_SIZE, color: BASE_COLOR };
-    var head = { type: "rectangle", x: xStart, y: yStart + SEG_SIZE * 8, width: SEG_SIZE, height: 0, color: BASE_COLOR };
+    var tail = { type: "rectangle", dir: DOWN, x: xStart, y: yStart + SEG_SIZE * 4, width: SEG_SIZE, height: SEG_SIZE, color: BASE_COLOR };
+    var s2 = { type: "rectangle", dir: DOWN, x: xStart, y: yStart + SEG_SIZE * 5, width: SEG_SIZE, height: SEG_SIZE, color: BASE_COLOR };
+    var s3 = { type: "rectangle", dir: DOWN, x: xStart, y: yStart + SEG_SIZE * 6, width: SEG_SIZE, height: SEG_SIZE, color: BASE_COLOR };
+    var s4 = { type: "rectangle", dir: DOWN, x: xStart, y: yStart + SEG_SIZE * 7, width: SEG_SIZE, height: SEG_SIZE, color: BASE_COLOR };
+    var head = { type: "rectangle", dir: DOWN, x: xStart, y: yStart + SEG_SIZE * 8, width: SEG_SIZE, height: 0, color: BASE_COLOR };
     return [tail, s2, s3, s4, head];
 }
 
@@ -29,41 +29,111 @@ function getSegments(canvas) {
 //It does not respond to events. But the events are loaded and ready to use.
 export function updateSnake(game) {
 
+    var canvas = game.canvas;
     var snake = game.snake;
+    var head = snake.parts[snake.parts.length - 1];
+    var tail = snake.parts[0];
 
     //register the next Direction.  We won't actually change directions until the current head gets to 100%
-    if (snake.nextDir = NONE) {
-        if (game.events.upPressed && (snake.dir == LEFT || snake.dir == RIGHT)) snake.nextDir = UP;
-        if (game.events.downPressed && (snake.dir == LEFT || snake.dir == RIGHT)) snake.nextDir = DOWN;
-        if (game.events.leftPressed && (snake.dir == UP || snake.dir == DOWN)) snake.nextDir = LEFT;
-        if (game.events.rightPressed && (snake.dir == UP || snake.dir == DOWN)) snake.nextDir = RIGHT;
+    if (snake.nextDir == NONE || snake.nextDir2 == NONE) {
+        if (game.events.upPressed && (head.dir == LEFT || head.dir == RIGHT)) {
+            if (snake.nextDir == NONE) snake.nextDir = UP;
+            else snake.nextDir2 = UP;
+            game.events.upPressed = false;
+        }
+        if (game.events.downPressed && (head.dir == LEFT || head.dir == RIGHT)) {
+            if (snake.nextDir == NONE) snake.nextDir = DOWN;
+            else snake.nextDir2 = DOWN;
+            game.events.downPressed = false;
+        }
+        if (game.events.leftPressed && (head.dir == UP || head.dir == DOWN)) {
+            if (snake.nextDir == NONE) snake.nextDir = LEFT;
+            else snake.nextDir2 = LEFT;
+            game.events.leftPressed = false;
+        }
+        if (game.events.rightPressed && (head.dir == UP || head.dir == DOWN)) {
+            if (snake.nextDir == NONE) snake.nextDir = RIGHT;
+            else snake.nextDir2 = RIGHT;
+            game.events.rightPressed = false;
+        }
     }
 
     //make sure the head is not bigger than the segment size
     snake.headSize = snake.headSize + snake.speed;
     if (snake.headSize >= SEG_SIZE) {
         snake.headSize = SEG_SIZE;
-    } 
+    }
 
-    //subtract a little from the tail
-    var tail = snake.parts[0];
-    tail.y = snake.parts[0].y + snake.speed;
-    tail.height = SEG_SIZE - snake.headSize;
-    //add a little to the head
-    var head = snake.parts[snake.parts.length - 1];
-    head.height = head.height + snake.speed;
+    if (head.dir == UP) {
+        head.y = head.y - snake.speed;
+        head.height = head.height + snake.speed;
+    }
+    if (head.dir == DOWN) {
+        head.height = head.height + snake.speed;
+    }
+    if (head.dir == LEFT) {
+        head.x = head.x - snake.speed;
+        head.width = head.width + snake.speed;
+    }
+    if (head.dir == RIGHT) {
+        head.width = head.width + snake.speed;
+    }
 
-    //if the current head is 100% done, then chop of the tail piece (shift) and add a new head (push)
-    //shift removes the first element of an array
-    //push adds a new element to the end of the array
-    if (snake.headSize == SEG_SIZE) { 
+    if (tail.dir == UP) {
+        tail.height = SEG_SIZE - snake.headSize;
+    }
+    if (tail.dir == DOWN) {
+        tail.y = tail.y + snake.speed;
+        tail.height = SEG_SIZE - snake.headSize;
+    }
+    if (tail.dir == LEFT) {
+        tail.width = SEG_SIZE - snake.headSize;
+    }
+    if (tail.dir == RIGHT) {
+        tail.x = tail.x + snake.speed;
+        tail.width = SEG_SIZE - snake.headSize;
+    }
+
+    if (snake.headSize == SEG_SIZE) {
         snake.parts.shift();
-        var oldHead = snake.parts[snake.parts.length - 1];
-        var newHead = { type: "rectangle", x: oldHead.x, y: oldHead.y + SEG_SIZE, width: SEG_SIZE, height: 0, color: BASE_COLOR };
-        if (newHead.y > game.canvas.height) newHead.y = 0;
-        snake.parts.push(newHead);
         snake.headSize = 0;
-    } 
+
+        var newDir = head.dir;
+        if (snake.nextDir != NONE) {
+            newDir = snake.nextDir;
+            snake.nextDir = snake.nextDir2;
+            snake.nextDir2 = NONE;
+        }
+        head.dir = newDir;
+
+        var newHead = { type: "rectangle", dir: newDir, x: head.x, y: head.y, width: SEG_SIZE, height: SEG_SIZE, color: BASE_COLOR };
+
+        if (newHead.dir == UP) {
+            newHead.height = 0;
+            if (newHead.y == 0) newHead.y = canvas.height; 
+            else newHead.y = newHead.y;
+        }
+        
+        if (newHead.dir == DOWN) {
+            newHead.height = 0;
+            if (newHead.y == canvas.height - SEG_SIZE) newHead.y = 0; 
+            else newHead.y = newHead.y + SEG_SIZE;
+        }
+        
+        if (newHead.dir == LEFT) {
+            newHead.width = 0;
+            if (newHead.x == 0) newHead.x = canvas.width; 
+            else newHead.x = newHead.x;
+        }
+        
+        if (newHead.dir == RIGHT) {
+            newHead.width = 0;
+            if (newHead.x == canvas.width - SEG_SIZE) newHead.x = 0; 
+            else newHead.x = newHead.x + SEG_SIZE;
+        }
+            
+        snake.parts.push(newHead);
+    }
 
     return snake;
 }
